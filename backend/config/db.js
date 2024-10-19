83fc0a28b55a30c4aea366052166ec168088ae20
Viraaj Singh Kochar <virxxjkochar@gmail.com>
2024-10-20 00:21:17 +0100

    db.js using MongoDB

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,20 +1,19 @@
-const mongoose = require('mongoose');
-require('dotenv').config(); // This will load environment variables from the .env file
+const { MongoClient } = require('mongodb');
+require('dotenv').config(); // To load environment variables
 
-const uri = process.env.MONGODB_URI; // MongoDB URI from the .env file
+const uri = process.env.MONGODB_URI;
 
-// Function to connect to MongoDB using Mongoose
 async function connectDB() {
-  try {
-    await mongoose.connect(uri, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log("Successfully connected to MongoDB using Mongoose!");
-  } catch (err) {
-    console.error("Failed to connect to MongoDB:", err);
-    process.exit(1); // Exit process with failure
-  }
+    const client = new MongoClient(uri, { useUnifiedTopology: true });
+    
+    try {
+        await client.connect();
+        console.log("Successfully connected to MongoDB!");
+        return client;  // Return the connected client to use in other files
+    } catch (err) {
+        console.error("Failed to connect to MongoDB", err);
+        process.exit(1);
+    }
 }
 
 module.exports = connectDB;
